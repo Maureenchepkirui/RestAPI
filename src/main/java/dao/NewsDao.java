90447144b919c05d6086837006825ef86fe37d68
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-22 17:57:37 +0300

    Implement interface

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,24 @@
 package dao;
 
+import models.Department_News;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+
+    void addNews(News news);
+    void addDepartmentNews(Department_News department_news);
+
+    //read
+    List<News> getAll();
+
+    News findById(int id);
+
+    //update
+
+    //delete
+
+    void clearAll();
 }
