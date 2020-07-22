dc0a4868b947907bd0f92965d92499f343a60567
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-22 18:03:08 +0300

    Import required dependencies

--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -1,6 +1,16 @@
 package dao;
 
 
+import org.junit.Before;
+import org.sql2o.Connection;
 
 class Sql2oNewsDaoTest {
+    private static Sql2oDepartmentsDao sql2oDepartmentsDao;
+    private static Sql2oUsersDao sql2oUsersDao;
+    private static Sql2oNewsDao sql2oNewsDao;
+    private static Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+}
 
