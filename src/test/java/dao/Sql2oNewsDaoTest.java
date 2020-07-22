f2a52973f747afc2ca7a4cfa17fc4952b34cc8de
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-22 18:02:19 +0300

    Remove unnecessary import statements

--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -1,14 +1,6 @@
 package dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class Sql2oNewsDaoTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-}
