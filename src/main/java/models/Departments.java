edb898ec2ae25bd347f25f109c8d86a9b5e250e5
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-22 17:51:12 +0300

    Create properties and constructors

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,57 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
+
+    private String name;
+    private String description;
+    private int id;
+    private int size;
+
+
+
+    public Departments(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.size=0;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departments that = (Departments) o;
+        return id == that.id &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
 }
