5f6e9b63e0fb75e7521ed59c7b56b20eecf1275f
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-22 20:00:26 +0300

    Create a helper for the purpose of testing

--- src/test/java/dao/Sql2oUsersDaoTest.java
+++ src/test/java/dao/Sql2oUsersDaoTest.java
@@ -1,7 +1,19 @@
 package dao;
 
+import models.Departments;
+import models.Users;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oUsersDaoTest {
 
+}
+//helper
+private Departments setUpNewDepartment() {
+    return new Departments("Kopaloans", "microfinance");
+}
+
+    private Users setUpNewUser() {
+        return new Users("Maureen", "Manager", "Managing Director");
+    }
 }
