b9cfb644dadfe7118a43ce825c6229a73d004add
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-22 20:02:34 +0300

    Remove unnecessary imports causing errors and correct indentation and spacing

--- src/test/java/dao/Sql2oUsersDaoTest.java
+++ src/test/java/dao/Sql2oUsersDaoTest.java
@@ -3,11 +3,11 @@ package dao;
 import models.Departments;
 import models.Users;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class Sql2oUsersDaoTest {
 
-}
+
 //helper
 private Departments setUpNewDepartment() {
     return new Departments("Kopaloans", "microfinance");
