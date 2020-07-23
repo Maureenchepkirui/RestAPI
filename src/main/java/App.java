9f169405bb49a081df9ab2d8cb04f28a4926b919
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-23 09:19:35 +0300

    Import necessary files and set static file location to public folder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,12 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Connection;
+
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
 public class App {
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
@@ -6,4 +15,13 @@ public class App {
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        Sql2oNewsDao sql2oNewsDao;
+        Sql2oUsersDao sql2oUsersDao;
+        Sql2oDepartmentsDao sql2oDepartmentsDao;
+        Connection conn;
+        Gson gson = new Gson();
+        staticFileLocation("/public");
 }
