193883a8187e9475623f5ceff876fc1221fb7a22
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-23 09:18:19 +0300

    Fixind database issues and getting heroku assigned port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,9 @@
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
