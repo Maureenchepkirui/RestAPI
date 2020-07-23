09281352372a2d925ef0ccb399d4b3b51bf3ceae
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-23 12:49:46 +0300

    Remove the property departmentId since it isn't necessary

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -8,14 +8,12 @@ public class Users {
     private  String name;
     private String position;
     private String staff_role;
-    private int departmentId;
 
 
-    public Users(String name, String position, String staff_role,int departmentId) {
+    public Users(String name, String position, String staff_role) {
         this.name = name;
         this.position = position;
         this.staff_role = staff_role;
-        this.departmentId = departmentId;
 
     }
 
@@ -24,37 +22,27 @@ public class Users {
     }
 
     public String getName() {
-
         return name;
     }
 
-    public String getPosition()
-    {
+    public String getPosition() {
         return position;
     }
 
     public String getStaff_role() {
-
         return staff_role;
-    }
-
-    public int getDepartmentId() {
-        return departmentId;
-    }
-    public void setDepartmentId(int departmentId) {
-        this.departmentId = departmentId;
     }
 
     public void setId(int id) {
         this.id = id;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Users users = (Users) o;
         return id == users.id &&
-                departmentId == users.departmentId &&
                 Objects.equals(name, users.name) &&
                 Objects.equals(position, users.position) &&
                 Objects.equals(staff_role, users.staff_role) ;
@@ -63,6 +51,6 @@ public class Users {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, position, staff_role,departmentId);
+        return Objects.hash(id, name, position, staff_role);
     }
 }
